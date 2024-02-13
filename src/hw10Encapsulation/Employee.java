f7f60644958790072fd53bb0e660a7e3d4591d4f
NadiaJara <nadiajara2011@gmail.com>
2024-02-13 18:05:54 -0500

    hw 11 if Else condition

--- src/hw10Encapsulation/Employee.java
+++ src/hw10Encapsulation/Employee.java
@@ -2,4 +2,41 @@ package hw10Encapsulation;
 
 public class Employee {
 
+	private String name;
+	private int age;
+	private char gender;
+	private boolean citizen;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public boolean getCitizen() {
+		return citizen;
+	}
+
+	public void setCitizen(boolean citizen) {
+		this.citizen = citizen;
+	}
+
 }
--- src/hw10Encapsulation/EmployeeTest.java
+++ src/hw10Encapsulation/EmployeeTest.java
@@ -2,4 +2,14 @@ package hw10Encapsulation;
 
 public class EmployeeTest {
 
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		employee.setName("Nadia Jara");
+		employee.setAge(12);
+		employee.setGender('F');
+		employee.setCitizen(true);
+
+		System.out.println("Name: " + employee.getName() + "\nAge: " + employee.getAge() + "\nGender: "
+				+ employee.getGender() + "\nI am US Citizen: " + employee.getCitizen());
+	}
 }
