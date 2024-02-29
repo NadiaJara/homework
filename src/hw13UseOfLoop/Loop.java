4a2a573e6890b04cfcd3230a6e97ed500b1eadb4
NadiaJara <nadiajara2011@gmail.com>
2024-02-29 16:21:53 -0500

    hw 8 Loops

---
+++ src/hw13UseOfLoop/Loop.java
@@ -0,0 +1,15 @@
+package hw13UseOfLoop;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		for (int i = -20; i <= 50; i += 1) {
+			if (i % 2 == 1) {
+				System.out.println("Odd Number: " + i);
+			} else {
+				
+			}
+			
+		}
+	}
+}
